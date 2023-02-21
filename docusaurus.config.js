f20421307b5e1d9ff0ff3f59473fd0701c943259
richie <rdcordrey@gmail.com>
2023-02-21 16:50:17 -0500

    init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,14 @@ const config = {
   url: 'https://ii-richie.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/build/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ii-richie', // Usually your GitHub org/user name.
   projectName: 'ii-richie.github.io', // Usually your repo name.
+
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
